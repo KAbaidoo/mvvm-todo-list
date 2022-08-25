06435eb85c0df84539255890cf1759dd22b6ea70
Kelvin Abaidoo <kabaidoo@hotmail.com>
2022-08-25 18:07:57 +0000

    created DataStore

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TaskFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TaskFragment.kt
@@ -8,12 +8,16 @@ import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
 import com.codinginflow.mvvmtodo.data.SortOrder
 import com.codinginflow.mvvmtodo.databinding.FragmentTasksBinding
 import com.codinginflow.mvvmtodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TaskFragment : Fragment(R.layout.fragment_tasks) {
@@ -46,6 +50,11 @@ class TaskFragment : Fragment(R.layout.fragment_tasks) {
 
         searchView.onQueryTextChanged {
             viewModel.searchQuery.value = it
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_tasks).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
         }
 
     }
